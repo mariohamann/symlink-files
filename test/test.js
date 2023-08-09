59c4159366a7fbf8f73718a7d6827bae4d5e80aa
Mario Hamann <mario.hamann@virtual-identity.com>
2023-08-09 15:35:11 +0200

    test: 🧪 optimize test.js

--- test/test.js
+++ test/test.js
@@ -75,23 +75,18 @@ async function runTests(useCLI = false) {
   console.assert(!await checkIfSymlinks('folder/nested-file.txt'), 'nested-file.txt symlink should be removed after removeSymlinks');
   console.assert(await checkIfDirExists('folder'), 'folder should not be removed in default state');
 
-  // 3. createSymlinks
+  // 3. links can be persisted
   if (useCLI) {
     await execPromise(`${cliPath} create -s ${source} -t ${target} -g ${globs}`);
-  } else {
-    await createSymlinks(config);
-  }
-
-  // 4. persistSymlinks
-  if (useCLI) {
     await execPromise(`${cliPath} persist -t ${target} -g ${globs}`);
   } else {
+    await createSymlinks(config);
     await persistSymlinks(config);
   }
   console.assert(await checkIfFileExists('file.txt'), 'file.txt should be a real file after persistSymlinks');
   console.assert(await checkIfFileExists('folder/nested-file.txt'), 'nested-file.txt should be a real file after persistSymlinks');
 
-  // 5. createSymlinks (no override by default)
+  // 4. createSymlinks (no override by default)
   if (useCLI) {
     await execPromise(`${cliPath} create -s ${source} -t ${target} -g ${globs}`);
   } else {
@@ -100,7 +95,7 @@ async function runTests(useCLI = false) {
   console.assert(await checkIfFileExists('file.txt'), 'file.txt should still be a real file after createSymlinks without override');
   console.assert(await checkIfFileExists('folder/nested-file.txt'), 'nested-file.txt should still be a real file after createSymlinks without override');
 
-  // 6. createSymlinks with override
+  // 5. createSymlinks with override
   config[0].overrideFiles = true;
   if (useCLI) {
     await execPromise(`${cliPath} create -s ${source} -t ${target} -g ${globs} -o`);
@@ -110,7 +105,7 @@ async function runTests(useCLI = false) {
   console.assert(await checkIfSymlinks('file.txt'), 'file.txt should be a symlink after createSymlinks with override');
   console.assert(await checkIfSymlinks('folder/nested-file.txt'), 'nested-file.txt should be a symlink after createSymlinks with override');
 
-  // 7. removeSymlinks
+  // 6. removeSymlinks
   if (useCLI) {
     await execPromise(`${cliPath} remove -t ${target} -g ${globs}`);
   } else {
@@ -119,19 +114,13 @@ async function runTests(useCLI = false) {
   console.assert(!await checkIfSymlinks('file.txt'), 'file.txt symlink should be removed after final removeSymlinks');
   console.assert(!await checkIfSymlinks('folder/nested-file.txt'), 'nested-file.txt symlink should be removed after final removeSymlinks');
 
-  // 8. Test for removeEmptyDirs
-  // Re-create symlinks first
+  // 7. Test for removeEmptyDirs
   if (useCLI) {
     await execPromise(`${cliPath} create -s ${source} -t ${target} -g ${globs}`);
-  } else {
-    await createSymlinks(config);
-  }
-
-  // As we don't have a direct CLI flag for removeEmptyDirs, it's assumed you'll manage that configuration outside the test for CLI tests.
-  config[0].removeEmptyDirs = true;
-  if (useCLI) {
     await execPromise(`${cliPath} remove -t ${target} -g ${globs} --removeEmptyDirs`);
   } else {
+    await createSymlinks(config);
+    config[0].removeEmptyDirs = true;
     await removeSymlinks(config);
   }
   console.assert(!await checkIfDirExists('folder'), 'folder should be removed after removeSymlinks with removeEmptyDirs set to true');
